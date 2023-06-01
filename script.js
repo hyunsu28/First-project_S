d7e2de1743e56d58a8b9cd363422961c35d56268
Raptor\Owner <hyunsu5190@gmail.com>
2023-06-02 02:52:37 +0900

    수정?

---
+++ script.js
@@ -0,0 +1,93 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNzk5ZDNkOTkyN2ZmZjZhMzk5MjdhMDZkNjFhNDRhZiIsInN1YiI6IjY0NzViNGEzZGQyNTg5MDBlMjBjYjZhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bi1e5A7NcsbeFyfdrKSbYSD1oBwJFEU0pk0OVi1lypU'
+  }
+};
+
+const moviesContainer = document.getElementById('movies');
+
+// 영화 정보 카드 생성 함수
+function createMovieCard(movie) {
+  const movieCard = document.createElement('div');
+  movieCard.classList.add('card');
+
+  const movieImage = document.createElement('img');
+  movieImage.src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+  movieImage.alt = movie.title;
+  movieImage.classList.add('card-image');
+
+  const movieTitle = document.createElement('div');
+  movieTitle.classList.add('card-title');
+  movieTitle.textContent = movie.title;
+
+  const movieOverview = document.createElement('div');
+  movieOverview.classList.add('card-text');
+  movieOverview.textContent = movie.overview;
+
+  movieCard.appendChild(movieImage);
+  movieCard.appendChild(movieTitle);
+  movieCard.appendChild(movieOverview);
+
+  // 영화 포스터 클릭 시 팝업으로 id 보여주기
+  movieImage.addEventListener('click', () => {
+    alert(`영화 ID: ${movie.id}`);
+  });
+
+  return movieCard;
+}
+
+// API 요청 및 영화 정보 카드 생성
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+  .then(response => response.json())
+  .then(response => {
+    const movies = response.results;
+    movies.forEach(movie => {
+      const movieCard = createMovieCard(movie);
+      moviesContainer.appendChild(movieCard);
+    });
+  })
+  .catch(err => console.error(err));
+
+// 영화 검색 기능 구현
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+function fetchMovies(url) {
+  fetch(url, options)
+    .then(response => response.json())
+    .then(response => {
+      const movies = response.results;
+      moviesContainer.innerHTML = ''; // 기존의 영화 카드 제거
+      movies.forEach(movie => {
+        const movieCard = createMovieCard(movie);
+        moviesContainer.appendChild(movieCard);
+      });
+    })
+    .catch(err => console.error(err));
+}
+
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value;
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&language=en-US&page=1&include_adult=false`;
+  if (searchTerm.trim() !== '') {
+    fetchMovies(searchUrl);
+  } else {
+    const initialUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+    fetchMovies(initialUrl);
+  }
+});
+
+
+    // 검색어가 비어있을 경우 모든 영화를 보여줌
+    allMovies.forEach(movie => {
+      const movieCard = createMovieCard(movie);
+      moviesContainer.appendChild(movieCard);
+    });
+
+// // Initial fetch when the page loads
+// window.addEventListener('DOMContentLoaded', () => {
+//   const initialUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+//   fetchMovies(initialUrl);
+// });
